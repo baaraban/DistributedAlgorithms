a9f68dd8082eb3def1b27290c17743ce606a354a
baaraban <perepichka@ucu.edu.ua>
2018-06-04 19:38:09 +0300

    Add base for distributed computing simulation

---
+++ Infrastructure/DistributedSystemSimulation/DistributedNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DistributedSystemSimulation
+{
+	public class DistributedNode
+	{
+		protected object data;
+		protected Func<object, object> job;
+
+		public DistributedNode(object data, Func<object, object> job)
+		{
+			this.data = data;
+			this.job = job;
+		}
+
+		public object DoWork(object parameter)
+		{
+			var returnTask = new Task<object>(() => this.job(parameter));
+			returnTask.Start();
+			return returnTask.Result;
+		}
+	}
+}
---
+++ Infrastructure/DistributedSystemSimulation/MainHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.DistributedSystemSimulation 
+{
+	public class MainHandler<NodeType> where NodeType: DistributedNode
+	{
+		private int amountOfNodes;
+		private List<NodeType> nodes = new List<NodeType>();
+
+		public int AmountOfNodes { get { return amountOfNodes; } }
+
+		public MainHandler(int amountOfNodes, List<object> dataset, Func<object, object> jobForAll)
+		{
+			if(amountOfNodes != dataset.Count)
+			{
+				throw new ArgumentException("Input parameters mismatch");
+			}
+			foreach(var d in dataset)
+			{
+				nodes.Add((NodeType)Activator.CreateInstance(
+					typeof(NodeType), 
+					new object[] { d, jobForAll }
+					));
+			}
+		}
+
+		public MainHandler(int amountOfNodes, List<Tuple<object, Func<object, object>>> datasetWithJobs)
+		{
+			if (amountOfNodes != datasetWithJobs.Count)
+			{
+				throw new ArgumentException("Input parameters mismatch");
+			}
+			foreach (var dj in datasetWithJobs)
+			{
+				nodes.Add((NodeType)Activator.CreateInstance(
+					typeof(NodeType),
+					new object[] { dj.Item1, dj.Item2}
+					));
+			}
+		}
+	}
+}
